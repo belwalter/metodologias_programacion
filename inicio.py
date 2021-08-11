8f61cf803d01dbff07ecf4b1aa716135dab6aadd
walter <belwalterv@gmail.com>
2021-08-10 22:00:46 -0300

    clase 1

--- inicio.py
+++ inicio.py
@@ -1,3 +1,65 @@
 
+#! TIPOS DE DATOS STR - NUMERICOS (INT , FLOAT, COMPLEX), BOOL, NONE
 
-print("hola mundo")
+numero = "hola"
+print(type(numero))
+
+numero = 105
+print(type(numero))
+
+numero = 41.07
+print(type(numero))
+
+numero = False
+print(type(numero))
+
+#! FUNCIONES DE CONVERSION INT() STR() FLOAT() BOOL()
+
+# name = input('ingrese su nombre ')
+# age = int(input('ingrese su edad '))
+
+# print(type(name), type(age))
+# print(name, 2021-age)
+
+#! OPERADORES ARITMETICOS + - * / // % **
+
+# print(2 + 3)
+# print(2 - 3)
+# print(2 * 3)
+# print(2 / 3)
+# print(2 // 3)
+# print(2 % 3)
+# print(2 ** 3)
+
+#! FLUJO DE CONTROL  CONDICIONALES (IF-IF ELSE-ANIDADOS), CICLOS (FOR-WHILE)
+#! OPERADORES DE CONTROL > < >= <= == !=
+
+numero = 0
+if(numero > 6):    
+    print('es mayor')
+
+
+if(numero % 2 == 0):
+    print('el numero es par')
+    
+else:
+    print('el numero es impar')
+
+
+if(numero > 0):
+    print('el numero es positivo')
+else:
+    if(numero < 0):
+        print('el numero es negativo')
+    else:
+        print('es neutro')
+
+
+if(numero > 0):
+    print('el numero es positivo')
+elif(numero < 0):
+    print('el numero es negativo')
+else:
+    print('es neutro')
+
+print('fin')
